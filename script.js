0583e19ea46a7d00675a2b217b7cf1252684d3d0
seifeslimene <s.slimene19@gmail.com>
2023-02-05 15:46:05 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+$(function() {
+  $( "button" ).click(function() {
+    $("p").toggle();
+    $("img").toggle();
+  });
+});
+// let images = document.getElementsByTagName("img")[20]
+// alert(images[images.length - 1]);
